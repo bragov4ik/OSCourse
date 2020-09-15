5abbb00c18ee77db46af682f55ae189ca9dc63fe
Kirill Ivanov <bragov4ik@gmail.com>
2020-09-15 15:50:06 +0300

    ex2 completed?

--- week5/ex2.sh
+++ week5/ex2.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+
 while :
 do
-NUMBER=$(tail -n 1 < ex2.txt)
-let NUMBER=$NUMBER+1
-echo $NUMBER >> ex2.txt
+
+    if ln ex2.txt ex2.txt.lock 
+    then
+        NUMBER=$(tail -n 1 < ex2.txt)
+        let NUMBER=$NUMBER+1
+        echo $NUMBER >> ex2.txt
+
+        rm ex2.txt.lock
+    fi
+
 done
+# the first race condition was number 653; it was printed 2 times at lines 653 and 654
+# the critical region is the body of while loop: we access the text file there.
+# what happens is 2 processes read the same number n, then print n+1
