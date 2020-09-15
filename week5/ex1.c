4d9931072fde85af551e7ae16830c90949d28588
Kirill Ivanov <bragov4ik@gmail.com>
2020-09-15 14:02:48 +0300

    ex1 v2

--- week5/ex1.c
+++ week5/ex1.c
@@ -22,11 +22,15 @@ int main() {
         else {
             printf("Main here! %d was just created! \n", thread_identifiers[i]);
         }
+        printf("Waiting for thread %d to complete...\n", thread_identifiers[i]);
+        pthread_join(thread_identifiers[i], NULL);
+        printf("%d has finished!\n \n", thread_identifiers[i]);
     }
-
+    /*
     for (int i = 0; i < N; i++) {
         pthread_join(thread_identifiers[i], NULL);
     }
+    */
 }
 
 /* Output:
@@ -42,4 +46,32 @@ Hello, I am a thread number 1397217024! "some text"!
 So here the threads were created, then they are performed in some order
 that is decided by the scheduler. They are not necessary executed one by one
 as demonstrated in the output: they wrote the text in order 1 2 4 3.
+*/
+
+/* Output 2:
+Main here! -1145202944 was just created! 
+Waiting for thread -1145202944 to complete...
+Hello, I am a thread number -1145202944! "some text"! 
+-1145202944 has finished!
+ 
+Main here! -1145202944 was just created! 
+Waiting for thread -1145202944 to complete...
+Hello, I am a thread number -1145202944! "some text"! 
+-1145202944 has finished!
+ 
+Main here! -1145202944 was just created! 
+Waiting for thread -1145202944 to complete...
+Hello, I am a thread number -1145202944! "some text"! 
+-1145202944 has finished!
+ 
+Main here! -1145202944 was just created! 
+Waiting for thread -1145202944 to complete...
+Hello, I am a thread number -1145202944! "some text"! 
+-1145202944 has finished!
+*/
+/*
+In this case all threads are performed sequentially as we explicitly wait for 
+them to finish one by one. Also I've observed an interesting fact:
+the thread identifier (pthread_t) is the same for every new created thread (after 
+exiting from the previous).
 */
